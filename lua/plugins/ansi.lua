0bc372a854bee0f15e09fd52d81e04e054249dcc
James Chew <james.chew@runpod.io>
2026-05-20 20:58:46 -0600

    feat: updated baleia and conform/prettier config

--- lua/plugins/ansi.lua
+++ lua/plugins/ansi.lua
@@ -1,6 +1,7 @@
 return {
   "m00qek/baleia.nvim",
   version = "*",
+  event = { "BufReadPre *.log", "BufNewFile *.log" },
   config = function()
     local baleia = require("baleia").setup({
       -- defaults are usually fine; optional tweaks:
@@ -12,6 +13,21 @@ return {
       pattern = { "*.log" },
       callback = function(args)
         baleia.once(args.buf)
+        -- Strip terminal control sequences that baleia doesn't handle
+        -- (cursor movement, line clearing, synchronized output, etc.)
+        local lines = vim.api.nvim_buf_get_lines(args.buf, 0, -1, false)
+        local dirty = false
+        for i, line in ipairs(lines) do
+          local cleaned = line:gsub("\27%[%??[%d;]*[A-Za-z]", "")
+          if cleaned ~= line then
+            lines[i] = cleaned
+            dirty = true
+          end
+        end
+        if dirty then
+          vim.api.nvim_buf_set_lines(args.buf, 0, -1, false, lines)
+          baleia.once(args.buf)
+        end
       end,
 
       -- used to colorize for streaming files
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,15 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    formatters = {
+      prettier = {
+        command = "node_modules/.bin/prettier",
+        cwd = function()
+          return vim.fs.root(0, { "package.json", ".git" })
+        end,
+        require_cwd = true,
+      },
+    },
     formatters_by_ft = {
       javascript = { "prettier" },
       typescript = { "prettier" },
